342d093dd5d18f47d5224704fe2d1bfec2768098
Seyed-Amirhossein <120096380+Seyed-Amirhossein@users.noreply.github.com>
2023-01-23 11:21:41 +0330

    Add files via upload

---
+++ Griffin_Airlines/src/main/java/com/example/griffin_airlines/GriffinAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.example.griffin_airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriffinAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriffinAirlinesApplication.class, args);
+	}
+
+}
